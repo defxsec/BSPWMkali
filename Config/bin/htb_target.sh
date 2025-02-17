dcc72051d17c81644536fce5bf78d618bcaf555e
defxsec <defxsec@gmail.com>
2025-02-16 21:52:34 -0400

    Configuraciones

--- Config/bin/htb_target.sh
+++ Config/bin/htb_target.sh
@@ -5,11 +5,11 @@ name_target=$(cat ~/.config/bin/target | awk '{print $2}')
 
 	if [ $ip_target ] && [ $name_target ]; then
 	#echo "%{F#a486dd}什 %{F#ffffff} $ip_target - $name_target"
-	echo "%{F#a486dd}什 $ip_target - $name_target%{F#ffffff}"
+	echo "%{F#8e24aa}什 $ip_target - $name_target%{F#ffffff}"
 	elif [ $(cat ~/.config/bin/target | wc -w) -eq 1 ]; then
 	#echo "%{F#a486dd}什%{F#ffffff} $ip_target"
-	echo "%{F#a486dd}什 $ip_target%{F#ffffff}"
+	echo "%{F#8e24aa}什 $ip_target%{F#ffffff}"
 	else
 	#echo "%{F#a486dd}ﲅ %{u-}%{F#ffffff} No target"
-	echo "%{F#a486dd}ﲅ %{u-} No target%{F#ffffff}"
+	echo "%{F#8e24aa}ﲅ %{u-} No target%{F#ffffff}"
 	fi
